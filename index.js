d3d09ac0f1701b8fcd38a3df526cd2e730f3b25f
saoda <jafrinsaoda@gmail.com>
2022-04-05 14:19:09 +0600

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/path', (req, res) => {
+  res.send('my saoda!')
+})
+
+
+app.listen(port, () => {
+  console.log(`my saoda is listening ${port}`)
+})
